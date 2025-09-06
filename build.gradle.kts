3f4a794ad140a9ccb74755b810c3e47dcddc8f2e
lukas <lukasjonsson@outlook.de>
2025-09-06 14:43:18 +0200

    Only use minimal opengl and minimal opengles

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,15 +26,16 @@ sourceSets {
 }
 
 dependencies {
-    implementation("com.github.javaparser:javaparser-core:3.26.2")
-    implementation("javax.annotation:javax.annotation-api:1.3.2")
+    compileOnly("com.github.javaparser:javaparser-core:3.26.2")
+    compileOnly("javax.annotation:javax.annotation-api:1.3.2")
     implementation("org.jetbrains:annotations:26.0.2-1")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     lwjgl {
         version = Release.latest
-        implementation(Preset.everything)
+        implementation(Preset.minimalOpenGL)
+        implementation(Preset.minimalOpenGLES)
     }
 }
 
@@ -45,11 +46,6 @@ tasks.test {
 group = "de.verdox.polylwjgl3"
 version = "1.0.0"
 java.sourceCompatibility = JavaVersion.VERSION_17
-
-java {
-    withSourcesJar()
-    withJavadocJar()
-}
 
 tasks.withType<Javadoc> {
     // Warnings/Errors ignorieren
